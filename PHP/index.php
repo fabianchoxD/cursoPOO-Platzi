b440e42809f244f0db9905828f66bd787c46e228
Fabian S. Varon Valencia <fabian.varon@cwc.com>
2022-09-06 11:57:16 -0500

    add an uberPool example

--- PHP/index.php
+++ PHP/index.php
@@ -7,3 +7,8 @@ require_once('uberPool.php');
 
 $uberX = new UberX("RTC46F", new Account("Fabian Varon", "AMS123"), "Audi", "2023");
 $uberX->printDataCar();
+
+$uberPool = new UberPool("CPW554", new Account("Cosme Fulanito", "1345ADC"), "Lamborgini", "2020");
+$uberPool->printDataCar();
+
+?>

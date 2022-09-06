b61d0a9497c775f7a860007203125473e9c16f1f
Fabian S. Varon Valencia <fabian.varon@cwc.com>
2022-09-06 15:36:17 -0500

    create uberX object. update passengr to use setter

--- Java/Main.java
+++ Java/Main.java
@@ -3,18 +3,19 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Car car = new Car("RTC46F", new Account("Fabian Varon", "1075269439"));
+        //Car car = new Car("RTC46F", new Account("Fabian Varon", "1075269439"));
+        UberX uberX = new UberX("RTC46F", new Account("Fabian Varon", "ABC123"), "porshe", "2023");
         //car.license = "RTC46F";
         //car.driver = "Fabian Varon";
-        car.passenger = 2;
+        uberX.setPassenger(3);
         //System.out.println("Car License: " + car.license);
-        car.printDataCar();
-
-        Car car2 = new Car("CPW554", new Account("Stevens", "1075269439"));
+        uberX.printDataCar();
+/*
+        Car car2 = new Car("CPW554", new Account("Stevens", "34DDbJ"));
         //car2.license = "CPW554";
         //car2.driver = "Stevens";
         car2.passenger = 5;
         //System.out.println("Car2 License: " + car2.license);
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }

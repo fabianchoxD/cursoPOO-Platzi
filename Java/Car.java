41be31de7530b89e015f411e86b44588dbc5291b
Fabian S. Varon Valencia <fabian.varon@cwc.com>
2022-09-06 15:35:11 -0500

    add setter & getter to passenger property

--- Java/Car.java
+++ Java/Car.java
@@ -4,15 +4,29 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
 
-    public Car(String license, Account driver){
+    public Car(String license, Account driver) {
         this.license = license;
         this.driver = driver;
 
     }
 
-    void printDataCar(){
-        System.out.println("License: " + license + ", Driver name: " + driver.name);
+    public Integer getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if (passenger == 4) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("Error: UberX soporta sólamente 4 pasajeros.");
+        }
+    }
+
+    void printDataCar() {
+        if (passenger != null) {
+            System.out.println("License: " + license + ", Driver name: " + driver.name + ", passengers: " + passenger);
+        }
     }
 }

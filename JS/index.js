0d15077dba8e8a80b17968a9ab529ef60dd39b6f
Fabian S. Varon Valencia <fabian.varon@cwc.com>
2022-09-06 12:34:32 -0500

    create an uberX object

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
-var car = new Car ("RTC46F", new Account("Fabian Varon", "1075269439"))
+var car = new Car ("RTC46F", new Account("Fabian Varon", "ABC123"))
 car.passenger = 4;
 car.printDataCar()
+
+var uberX = new UberX ("CPW554", new Account("Cosme fulanito", "6YG4D"))
+uberX.passenger = 6;
+uberX.printDataCar()

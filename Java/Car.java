32c4ef1adc5efc006070c5528ad833a92599f7e1
Fabian S. Varon Valencia <fabian.varon@cwc.com>
2022-09-06 16:09:01 -0500

    added setter & getter for missing properties

--- Java/Car.java
+++ Java/Car.java
@@ -1,15 +1,39 @@
 package Java;
 
 class Car {
-    Integer id;
-    String license;
-    Account driver;
+    private Integer id;
+    private String license;
+    private Account driver;
     private Integer passenger;
 
     public Car(String license, Account driver) {
         this.license = license;
         this.driver = driver;
 
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
     }
 
     public Integer getPassenger() {
